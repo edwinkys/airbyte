bf855b74d296c941fa040172f0b8439f6990bc1a
Edwin Kys <itsedwinkys@gmail.com>
2024-05-20 16:20:33 -0500

    style: setup linting with flake8

--- airbyte-integrations/connectors/destination-indexify/destination_indexify/run.py
+++ airbyte-integrations/connectors/destination-indexify/destination_indexify/run.py
@@ -8,6 +8,7 @@ import sys
 from airbyte_cdk.entrypoint import launch
 from .destination import DestinationIndexify
 
+
 def run():
     destination = DestinationIndexify()
     destination.run(sys.argv[1:])
